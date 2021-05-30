6ca7e11ba2d65abbe1bf54ddd6e8ad28047f5535
yiqiangjizhang <yiqiangjizhang@gmail.com>
2021-05-30 20:16:24 +0200

    Updated PID control

--- PID_control/PID_control.ino
+++ PID_control/PID_control.ino
@@ -11,7 +11,7 @@
 //PID constants
 double kp = 3; // Proportional contribution
 double ki = 4; // Integral contribution
-double kd = 2; // Derivative contribution
+double kd = 0; // Derivative contribution
 
 // Parameters
 unsigned long currentTime, previousTime; // Initialize current time and previous time as unsigned long
@@ -22,12 +22,12 @@ double cumError, rateError;              // Initialize the cumulative Error (Int
 
 char inputData = 0; // Bluetooth input data
 
+
 // Main setup
 void setup()
 {
     Serial1.begin(9600); // Initialize UART port for Bluetooth communication
     Serial1.println("Fine Control stablished");
-    setPoint = 0; // Desired angle to turn
 }
 
 // Main loop
@@ -38,13 +38,13 @@ void loop()
     // Check if serial is available
     if (Serial1.available() > 0)
     {
-        inputData = Serial1.read();
+        setPoint = Serial1.read(); // Desired angle to turn (YA LO TENEMOS COMO VARIABLE GLOBAL)
     }
 
     // TODO: check how input data is formatted (if direction + angle or just angle)
 
     // Compute PID control
-    output = computePID(inputData);
+    output = computePID(setPoint);
 
     // Delay 100 ms
     delay(100);
@@ -54,14 +54,28 @@ void loop()
 }
 
 // PID function
-double computePID(double inp)
+double computePID()
 {
+    read_IMU();
+    
+    // Percentage
+    volatile float IMU_deg_data_Z_perc = (IMU_deg_data_Z - 360)/(-360)*100;
+
+    OBC_data_value = OBC_data_value + IMU_deg_data_Z;
+    
+    if (OBC_data_value < 0) OBC_data_value += 360;
+    if (OBC_data_value >= 360) OBC_data_value -= 360; //Estas dos lineas contienen el valor del heading en 0-360 grados.
+    
+    // Transform angle to percentage
+    volatile float OBC_data_value_perc = (OBC_data_value - 360)/(-360)*100;
+
+
     // Time
     currentTime = millis();                             // Get current time
     elapsedTime = (double)(currentTime - previousTime); // Compute elapsed time from previous time computation
 
     // Errors
-    error = setPoint - inp;                        // Calculate error (Proportional)
+    error = OBC_data_value_perc - IMU_deg_data_Z_perc;       // Calculate error (Proportional)
     cumError += error * elapsedTime;               // Calculate the cumulative error (Integral)
     rateError = (error - lastError) / elapsedTime; // Calculate the rate of error (Derivative)
 
@@ -70,7 +84,18 @@ double computePID(double inp)
     PID_I = ki * cumError;  // Integral
     PID_D = kd * rateError; // Derivative
 
+    if (PID_P > 255) PID_P = 255;
+    if (PID_P < -255) PID_P = -255;
+    if (PID_I > 255) PID_I = 255;
+    if (PID_I < -255) PID_I = -255;
+    if (PID_D > 255) PID_D = 255;
+    if (PID_D < -255) PID_D = -255;
+
     double PID_output = PID_P + PID_I + PID_D; // PID control
+
+    if (PID_output > 255) PID_output = 255;
+    if (PID_output < -255) PID_output = -255;
+
 
     // Save current error and time for next iteration
     lastError = error;          // Save current error
